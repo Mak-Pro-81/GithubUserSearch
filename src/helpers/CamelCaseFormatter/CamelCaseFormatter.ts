4c4bb57035f295d8c4b9a4b872223d96763e62fc
MakPro <maks.prokopenko@oneplusone.solutions>
2022-11-21 09:58:36 +0200

    camelCaseFormatter removed

--- src/helpers/CamelCaseFormatter/CamelCaseFormatter.ts
+++
@@ -1,5 +0,0 @@
-export const camelCaseFormatter = (obj: object) => {
-  return Object.fromEntries(
-    Object.entries(obj).map(n => [n[0].replace(/_+(.)/g, (m, g1) => g1.toUpperCase()), n[1]])
-  );
-};
--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,2 +1,1 @@
-export { camelCaseFormatter } from './camelCaseFormatter';
 export { dateFormatter } from './dateFormatter';
